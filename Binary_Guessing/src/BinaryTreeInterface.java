e9564f09ed4450d6456a871ce9fd009e220cce6f
deboadeyela <49371012+deboadeyela@users.noreply.github.com>
2021-09-05 16:55:06 +0100

    Update BinaryTreeInterface.java

--- Binary_Guessing/src/BinaryTreeInterface.java
+++ Binary_Guessing/src/BinaryTreeInterface.java
@@ -2,18 +2,18 @@
 /**
  * An interface for the ADT binary tree.
  * 
- * @author Frank M. Carrano
+ * @author gbadebo
  * @version 2.0
  */
 public interface BinaryTreeInterface<T> extends TreeInterface<T>
 {
   /** Task: Sets an existing binary tree to a new one-node binary tree.
-   *  @param rootData   an object that is the data in the new tree’s root
+   *  @param rootData   an object that is the data in the new tree√ïs root
    */
   public void setTree(T rootData);
   
   /** Task: Sets an existing binary tree to a new binary tree.
-   *  @param rootData   an object that is the data in the new tree’s root
+   *  @param rootData   an object that is the data in the new tree√ïs root
    *  @param leftTree   the left subtree of the new tree
    *  @param rightTree  the right subtree of the new tree */
   public void setTree(T rootData, BinaryTreeInterface<T> leftTree,
